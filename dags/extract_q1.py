24826aa9a85ee80072bf921a3d7eee2c57a5273c
minju210 <juliana6190210@gmail.com>
2024-08-05 15:43:35 +0900

    aadsfase

--- dags/extract_q1.py
+++ dags/extract_q1.py
@@ -24,25 +24,42 @@ with DAG(
     description='movie',
     schedule="10 2 * * *",
     start_date=datetime(2021, 1, 1),
-    end_date=datetime(2024, 4, 30),
+    end_date=datetime(2021, 4, 30),
     catchup=True,
     tags=['movie', 'megabox', 'team'],
 ) as dag:
     
-    def extract():
-        from movie_extract.movie_e import req2df
-        df = req2df(load_dt=ds_nodash)
-        
+    def extract(**kwargs):
+        from movie_extract.movie_e import df2parquet
+        date = kwargs['ds_nodash']
+        df = df2parquet(load_dt=date)
+    
+    def branch_func(ds_nodash):
+        import os
+        home_dir = os.path.expanduser("~")
+        path = os.path.join(home_dir, f'megabox/tmp/movie_parquet/load_dt={ds_nodash}')
+        if os.path.exists(path):
+            return "rm.dir"
+        else:
+            return "movie.extract"
     
     start = EmptyOperator(task_id='start')
     
+    branch_op = BranchPythonOperator(
+            task_id="branch.op",
+            python_callable=branch_func
+    )
+
     t_extract = PythonVirtualenvOperator(
             task_id='movie.extract',
             python_callable=extract,
             requirements=["git+https://github.com/DE32megabox/extract.git@dev/d1.0.0"],
-            system_site_packages=False
+            system_site_packages=False,
+            trigger_rule='all_done'
     )
 
     end = EmptyOperator(task_id='end')
     
-    start >> t_extract >> end
+    start >> branch_op >> t_extract
+    branch_op >> rm_dir >> t_extract
+    t_extract >> end
